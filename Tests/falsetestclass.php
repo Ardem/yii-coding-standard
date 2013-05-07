6d17199e526126221d8cdc7bc72420e6f4fc39a0
Ardem <lunoxot@mail.ru>
2013-05-07 10:52:07 +0200

    New rules and test file

---
+++ Tests/falsetestclass.php
@@ -0,0 +1,21 @@
+ <?php
+
+class testClassController
+{
+
+	const test_CONSTANT = true;
+
+	public $Test_Property = true;
+
+	public function Test_Method() {
+		return true;
+        }
+
+	private function _Private_Method() {
+
+	}
+}
+
+class SecondClass {
+
+}
